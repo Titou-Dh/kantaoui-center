4a9da2bfca2fc3a5f93a31d4d40799b04d3227d7
Titou-Dh <titoudh1@gmail.com>
2024-07-23 22:26:24 +0100

    added more icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
 import Hero from "@/components/hero";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
     <section className="">
-      <Hero/>
-      
+      <Hero />
+      <Features />
     </section>
   );
 }
