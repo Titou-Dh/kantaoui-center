850ffa7610ee6ab565cfa0409dcf0e5eaa72b2d0
Titou-Dh <titoudh1@gmail.com>
2024-07-26 21:45:56 +0100

    added the map

--- components/Localisation.tsx
+++ components/Localisation.tsx
@@ -14,6 +14,15 @@ const Localisation = () => {
         restaurants amusement park and funfairs . tennis , diving club , leisure
         port , golf course (PGA classified) Night clubs, etc
       </p>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3232.427639943773!2d10.594703175601586!3d35.88754077252135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13020f759d21bd6b%3A0xfa895ac38f56d2f6!2sEl%20Kantaoui%20Center!5e0!3m2!1sen!2stn!4v1722026665066!5m2!1sen!2stn"
+        width="600"
+        height="450"
+        className="w-full rounded-2xl shadow-lg my-10"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 };
--- components/hero.tsx
+++ components/hero.tsx
@@ -4,8 +4,7 @@ const Hero = () => {
     return (
         <>
             <div className="bg-primaryBg text-white py-12 md:px-40 px-16 capitalize w-full">
-                <h1 className="text-5xl ">Open the door for a <br />
-                    spacious <span className="text-primary">living-</span></h1>
+                <h1 className="text-7xl font-bold ">Hotel <br/>el Kantaoui <span className="text-primary">Center</span></h1>
                 <p className="text-gray-400 py-8">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
                     Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
