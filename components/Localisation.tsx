d4555efb21df66157f27717fb36ba825c779e09d
Titou-Dh <titoudh1@gmail.com>
2024-08-06 16:46:13 +0100

    update location

--- components/Localisation.tsx
+++ components/Localisation.tsx
@@ -6,7 +6,7 @@ const Localisation = () => {
       <h1 className="text-4xl capitalize font-bold" data-aos="fade-right">
         Where you <span className="text-primary">Find us</span>
       </h1>
-      <p className="text-gray-400 mt-5" data-aos="fade-up" date-aos-duration="1000">
+      <p className="text-gray-400 mt-5" data-aos="fade-up" >
         EL Kantaoui Center situated in the heart of the first resort of Tunisia
         port el kantaoui IO mn drive to the city center of Sousse 15Km from
         international airport of Monastir and 30 Km from the international
