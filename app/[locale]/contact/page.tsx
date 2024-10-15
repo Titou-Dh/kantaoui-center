2d7fece29d7efc75cf914b3b00fbb1bc97ff5885
Titou-Dh <titoudh1@gmail.com>
2024-10-15 19:49:40 +0100

    translated contact page

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useTranslations } from "next-intl"
 
 export default function Component() {
+  const  t  = useTranslations('Contact');
   return (
     <div className="container flex items-center flex-col md:flex-row  justify-around  mx-auto px-5 lg:px-32 py-12 md:py-20 lg:py-24">
       <div className=" mx-auto space-y-8 w-full  md:w-1/2 ">
         <div className="">
           <h1 className="text-3xl text-primary font-bold tracking-tight sm:text-4xl">
-            Get in Touch
+            {t("title")}
           </h1>
           <p className="mt-4 text-muted-foreground">
-            Have a question or need assistance? Don't hesitate to reach out.
-            We're here to help.
+            {t("paragraph")}
           </p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
-            <h2 className="text-xl font-semibold mb-4">Address</h2>
+            <h2 className="text-xl font-semibold mb-4">{t('address')}</h2>
             <div className="space-y-2">
               <p>Hotel el Kantaoui Center</p>
               <p>
@@ -26,14 +27,14 @@ export default function Component() {
             </div>
           </div>
           <div>
-            <h2 className="text-xl font-semibold mb-4">Phone</h2>
+            <h2 className="text-xl font-semibold mb-4">{t("phone")}</h2>
             <div className="space-y-2">
-              <p>Phone: +216 22 777 320</p>
-              <p>WhatsApp/Phone: +216 97 275 789</p>
+              <p>{t("phone")}: +216 22 777 320</p>
+              <p>WhatsApp/{t("phone")}: +216 97 275 789</p>
             </div>
           </div>
           <div>
-            <h2 className="text-xl font-semibold mb-4">Email</h2>
+            <h2 className="text-xl font-semibold mb-4">{t("email")}</h2>
             <div className="space-y-2">
               <p>
                 <Link href="#" className="underline" prefetch={false}>
